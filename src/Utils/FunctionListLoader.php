5039c87bb45463d2029fa7d470ccf7658de05cdd
Shish <webmaster@shishnet.org>
2024-10-08 18:48:26 +0100

    Pull function list from shish/safe

--- src/Utils/FunctionListLoader.php
+++ src/Utils/FunctionListLoader.php
@@ -22,10 +22,12 @@ class FunctionListLoader
         if (self::$functions === null) {
             if (\file_exists(__DIR__.'/../../../safe/generated/functionsList.php')) {
                 $functions = require __DIR__.'/../../../safe/generated/functionsList.php';
+            } elseif (\file_exists(__DIR__.'/../../vendor/shish/safe/generated/functionsList.php')) {
+                $functions = require __DIR__.'/../../vendor/shish/safe/generated/functionsList.php';
             } elseif (\file_exists(__DIR__.'/../../vendor/thecodingmachine/safe/generated/functionsList.php')) {
                 $functions = require __DIR__.'/../../vendor/thecodingmachine/safe/generated/functionsList.php';
             } else {
-                throw new \RuntimeException('Could not find thecodingmachine/safe\'s functionsList.php file.');
+                throw new \RuntimeException('Could not find shish/safe\'s functionsList.php file.');
             }
             // Let's index these functions by their name
             self::$functions = \Safe\array_combine($functions, $functions);
